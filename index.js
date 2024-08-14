962c047b25e0645d105ffc5f04a8eb4335333274
Veronica Palau <veronicampalau@gmail.com>
2024-08-13 23:31:02 -0300

    palavras-repetidas

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 Exemplo: abacate = { a: 3, b: 1, c:1, t: 1, e: 1}
 */
 
-const palavra = "abacate";
+const palavra = "Abacate".toLowerCase();
+let letras = {}
 
-console.log(palavra.length);
-
+for(let i = 0; i < palavra.length; i++) {
+    if (letras[palavra[i]]){
+        letras[palavra[i]]++;
+    }else{
+        letras[palavra[i]] = 1;
+    }
+    
+}
+console.log(letras);
 
