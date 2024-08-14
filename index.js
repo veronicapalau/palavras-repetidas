e928bcdd1f67a95a375b0072bdbea1242e874fa1
Veronica Palau <veronicampalau@gmail.com>
2024-08-13 23:09:50 -0300

    exercicio-letras-repetidas

---
+++ index.js
@@ -0,0 +1,9 @@
+/*Precisamos mostrar quantas letras repetidas aparecem em uma palavra
+Exemplo: abacate = { a: 3, b: 1, c:1, t: 1, e: 1}
+*/
+
+const palavra = "abacate";
+
+console.log(palavra.length);
+
+
